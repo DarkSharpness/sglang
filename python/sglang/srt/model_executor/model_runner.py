d10803aeda97630842811f715100785d53e2580d
DarkSharpness <2040703891@qq.com>
2025-07-01 04:10:04 -0700

    revert: revert last commit

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -815,12 +815,10 @@ class ModelRunner:
         Returns:
             GetAllSerializedParametersReqOutput containing serialized parameters and model config.
         """
-        from sgl_kernel import cuda_utils # type: ignore
-
         try:
             serialized_parameters = {}
             for name, param in self.model.named_parameters(remove_duplicate=False):
-                serialized_parameters[name] = cuda_utils.share_ipc_tensor(param)
+                serialized_parameters[name] = MultiprocessingSerializer.serialize(param)
 
             return GetAllSerializedParametersReqOutput(
                 serialized_parameters=[serialized_parameters],
